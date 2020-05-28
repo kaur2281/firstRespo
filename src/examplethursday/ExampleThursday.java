923849c84c7df5af1115e029bb84435b8a7643be
harba <harba@192.168.0.25>
2020-05-28 10:29:37 -0400

    my frst commit

--- src/examplethursday/ExampleThursday.java
+++ src/examplethursday/ExampleThursday.java
@@ -17,6 +17,7 @@ public class ExampleThursday {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("thursday");
+        System.out.println("day");
     }
     
 }
